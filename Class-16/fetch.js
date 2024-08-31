cf97ec8e9c3f86408e74c6450f2901f7852dbd8a
Neeraj Chaudhary <neerajchaudhary@Neerajs-MacBook-Air.local>
2024-08-31 14:11:53 +0530

    Class-16 Apis

---
+++ Class-16/fetch.js
@@ -0,0 +1,103 @@
+// fetch() function
+// it use promises, then when you make a request using fetch api, it returns a promise
+// that resolves to the server's response and this allows you to use promise's .then()
+// method to handle the successful response and .catch method to handle any errors
+// that may occurs
+// and also you can use async-await syantx which provides a more concise and synchronous
+// looking way to handle promise
+
+// Response
+// response.json() -> parses the response body as json and returns a promise that resolves
+// to a javascript object
+
+// response.text() => returns a promise that resolves to the response body as plain text
+
+// response.blob() => returns a promise that resolves to a Blob object representing
+// the response body
+
+// response.arrayBuffer() => returns a promise that resolves to an ArrayBuffer
+// respresenting the response body
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   }),
+// })
+//   .then((response) => response.json())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+// const headers = {
+//   "Content-Type": "application/json",
+// };
+// const body = JSON.stringify({
+//   title: "foo",
+//   body: "bar",
+//   userId: 1,
+// });
+
+// const requestOptions = {
+//   method: "POST",
+//   headers,
+//   body,
+// };
+
+// fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
+//   .then((response) => response.json())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+// const headers = new Headers();
+// headers.append("Content-Type", "application/json");
+
+// const body = JSON.stringify({
+//   title: "foo",
+//   body: "bar",
+//   userId: 1,
+// });
+
+// const requestOptions = {
+//   method: "POST",
+//   headers,
+//   body,
+// };
+
+// fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
+//   .then((response) => response.json())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+// const request = new Request("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   }),
+// });
+
+// fetch(request)
+//   .then((response) => response.json())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
+
+const controller = new AbortController();
+
+const timeout = setTimeout(() => controller.abort(), 0);
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  signal: controller.signal,
+})
+  .then((response) => response.json())
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error))
+  .finally(() => clearTimeout(timeout));
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ https://excalidraw.com/#json=6rP9fgqZeChDMNBG9hrk3,6oB3xTPqoT_otG9xj5etAw
 # reflow visualization of mozilla.org
 
 https://www.youtube.com/watch?v=ZTnIxIA5KGw
+
+https://excalidraw.com/#json=N2JChGhPf6QjNmeWMaD9g,SZn8_924DEO11RHjnopNog
